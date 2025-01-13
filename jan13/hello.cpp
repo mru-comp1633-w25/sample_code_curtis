aec2b1a5880dfd2f1c2c0259b83d574c1bcb4155
Charlotte Curtis <ccurtis@mtroyal.ca>
2025-01-13 11:16:25 -0700

    Adding Jan 13 demo code

---
+++ jan13/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello! Foo!\n";
+    string str = "a"; // don't use string in this course
+    char c = 'c';
+    cout << "Char c: " << c << endl;
+
+    double y = 5 / 2 + 1.0;
+    cout << "y: " << y << endl;
+    return 0;
+}
