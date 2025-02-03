b8b241c0411da13ffad5d5d36d2e33deb8dba062
Charlotte Curtis <ccurtis@mtroyal.ca>
2025-02-03 09:41:31 -0700

    Adding template structure

---
+++ template/main.cpp
@@ -0,0 +1,8 @@
+#include "mylib.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
---
+++ template/mylib.h
@@ -0,0 +1,8 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+// function declarations
+
+// Constants
+
+#endif // MYLIB_H
