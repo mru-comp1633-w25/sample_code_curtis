bcfcd254cfc8598839dd2f8d934d2751a9857269
Charlotte Curtis <ccurtis@mtroyal.ca>
2025-02-05 11:15:14 -0700

    A few more things from class

--- feb05/main.cpp
+++ feb05/main.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 
 int main() {
-    double high_temps[MAX_SIZE];
-    int n_temps;
-    read_temps(high_temps, n_temps);
-    print(high_temps, n_temps);
+    // double high_temps[MAX_SIZE];
+    // int n_temps;
+    // read_temps(high_temps, n_temps);
+    // print(high_temps, n_temps);
     
-    // char connect4[ROWS][COLS] = {};
-    // initialize(connect4);
-    // print(connect4);
+    char connect4[ROWS][COLS] = {};
+    initialize(connect4);
+    print(connect4);
+
+    // doesn't work like this
+    for (int i = 0; i < ROWS * COLS; ++i) {
+        cout << connect4[i] << ' ';
+    }
+    cout << endl;
+    
     return 0;
 }
